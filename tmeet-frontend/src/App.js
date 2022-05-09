fe4685d05c4b459fa64dc37ad5f28890f1d2c7b0
ssook1222 <chlthr1222@naver.com>
2022-05-10 05:26:30 +0900

    UPDATE : MUI 패키지 설치

--- tmeet-frontend/src/App.js
+++ tmeet-frontend/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Main from "./pages/Main";
+import Main from "./pages/main/Main";
+import Login from "./pages/login/Login"
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route>
-                    <Route exact path="/" element={<Main />}></Route>
+                    <Route exact path="/" element={<Main/>}></Route>
+                    <Route exact path="/login" element={<Login/>}></Route>
                 </Route>
             </Routes>
         </BrowserRouter>
---
+++ tmeet-frontend/src/pages/login/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './LoginMain.css';
+import { Card } from '@mui/material';
+
+function Login() {
+    return (
+        <div>
+            <body className="back">
+            <Card className="loginCard" variant="outlined"></Card>
+            </body>
+
+        </div>
+
+    );
+}
+
+export default Login;
