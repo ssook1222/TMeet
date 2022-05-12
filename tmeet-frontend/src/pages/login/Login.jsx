447e10096bd64ed8dce44a0e5a607dce17d6f2e6
ssook1222 <chlthr1222@naver.com>
2022-05-13 05:30:18 +0900

    UPDATE : 구글 웹 폰트 세팅

--- tmeet-frontend/src/pages/login/Login.jsx
+++ tmeet-frontend/src/pages/login/Login.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './LoginMain.css';
-import { Card } from '@mui/material';
+import {Card} from '@mui/material';
+
 
 function Login() {
     return (
         <div>
             <body className="back">
-            <Card className="loginCard" variant="outlined"></Card>
+                <div className="test"></div>
+                <Card className="loginCard" variant="outlined">
+                    <h1>로고</h1>
+                    <h3 style={{color:"#656262"}}>티밋을 사용하시려면 로그인이 필요합니다.</h3>
+                </Card>
             </body>
-
         </div>
 
     );
