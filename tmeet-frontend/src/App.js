7297cb1c836fa2e7b5bc07709b1dc94155decb0b
lucy <lucy1287@naver.com>
2022-05-20 20:42:24 +0900

    UPDATE : time 페이지

--- tmeet-frontend/src/App.js
+++ tmeet-frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Main from "./pages/main/Main";
-import Login from "./pages/login/Login"
+import Login from "./pages/login/Login";
+import Time from "./pages/time/Time";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Route>
                     <Route exact path="/" element={<Main/>}></Route>
                     <Route exact path="/login" element={<Login/>}></Route>
+                    <Route exact path="/time" element={<Time/>}></Route>
                 </Route>
             </Routes>
         </BrowserRouter>
