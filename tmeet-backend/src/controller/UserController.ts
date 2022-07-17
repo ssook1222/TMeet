5c6b8c1cee04ed26369885f1aad03c1ace2e3b9e
ssook1222 <chlthr1222@naver.com>
2022-07-17 14:57:49 +0900

    ADD : 로그인 api 추가

--- tmeet-backend/src/controller/UserController.ts
+++ tmeet-backend/src/controller/UserController.ts
@@ -17,4 +17,32 @@ export class UserController{
         res.status(200).send({check:"회원가입에 성공하였습니다."});
     }
 
+    static findUser = async (req, res) => {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        if(email==undefined && password!=undefined) {
+            return res.status(200).send({loginSuccess:false, message:"이메일 값이 전송되지 않았습니다."});
+        }
+        else if(email!=undefined && password==undefined){
+            return res.status(200).send({loginSuccess:false, message:"비밀번호 값이 전송되지 않았습니다."});
+        }
+
+        else if(email==undefined && password==undefined){
+            return res.status(200).send({loginSuccess:false, message:"이메일, 비밀번호 값이 전송되지 않았습니다."});
+        }
+
+        const result = await getConnection().getRepository(User).findOne({where:{email, password}});
+        console.log(result);
+
+        if(email!=undefined&&password!=undefined){
+            if(result!=null){
+                return res.status(200).send({loginSuccess:true, message:"로그인에 성공하였습니다."});
+            }
+            else{
+                return res.status(200).send({loginSuccess:false, message:"로그인에 실패하였습니다."});
+            }
+        }
+
+    }
 }
--- tmeet-backend/src/router/index.ts
+++ tmeet-backend/src/router/index.ts
@@ -4,5 +4,6 @@ import {UserController} from "../controller/UserController";
 const routes = Router();
 
 routes.post('/sign-up', UserController.addUser);
+routes.post('/log-in', UserController.findUser);
 
 export default routes;
