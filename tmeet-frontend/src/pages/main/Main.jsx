ac89da1b375fed3ca9abb173403fcd25ef3f15f0
SangEunPark23 <101485039+SangEunPark23@users.noreply.github.com>
2022-07-22 02:47:47 +0900

    [feature/main] UPDATE : 메인페이지 이미지 및 title weight 수정

--- tmeet-frontend/src/pages/main/Main.jsx
+++ tmeet-frontend/src/pages/main/Main.jsx
@@ -3,29 +3,24 @@ import React from "react";
 import './Main.css';
 // import meeting from '../'
 
-function App() {
+function mainApp() {
     return (
         <body>
-            <div className="App">
-                <NavBar></NavBar>
+        <div className="App">
+            <NavBar></NavBar>
+        </div>
+        <div className= "wrap">
+            <div className="mainTitle">
+                Team IT ?  T Meet !
             </div>
-           <div className= "wrap">
-                   <div className="title">
-                       Team IT ?  T Meet !
-                       <h6>모임을 정하는 가장 쉬운 방법</h6>
-                   </div>
-                   {/*<div className= "meeting">*/}
-                   {/*    <img src="img/meeting.png"></img>*/}
-                   {/*</div>*/}
-               <div className= "meeting">
-                   <img sx={{ m: 1 }}
-                        style={{marginBottom:"10px"}}
-                        src="img/meeting.png"></img>
-               </div>
-           </div>
+            <a className="mainContent">모임을 정하는 가장 쉬운 방법</a>
+        </div>
+        <div className="meeting-img">
+            <img src="img/meeting-img.png"></img>
+        </div>
 
         </body>
     );
 }
 
-export default App;
+export default mainApp;
