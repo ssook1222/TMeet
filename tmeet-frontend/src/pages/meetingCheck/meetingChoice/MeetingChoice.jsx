f0325f1252ddd8af1d69582402ee215c586146e3
SangEun123 <101400455+SangEun123@users.noreply.github.com>
2022-08-27 19:10:31 +0900

    [feature/weather] UPDATE : weather 프론트엔드-백엔드 연동, 날씨 아이콘 에러 (#64)
    * [feature/main] UPDATE : 메인페이지 이미지 및 title 수정
    
    * [feature/check] UPDATE : check, choice페이지 작성 및 네비바 연결
    
    * FIX : 버튼 클릭 시 meeting-choice로 이동 안 되는 부분 수정
    
    * [feature/check] UPDATE : 카카오톡 공유하기 api 구현
    
    * UPDATE : Meeting check 및 choice 수정, 카카오톡 공유하기 api 구현
    
    * FIX : tsx 세팅 변경
    
     Can't resolve './pages/place/PlaceMain' 오류 해결
    - ts와 js 혼용하여 발생하는 문제
    
    * weatherController 오류 수정
    
    * Fix : meeting-choice page bug fixed
    
    * [Feature/weather] UPDATE : weather 기능 구현
    
    * FIX : Backend Error Fix
    
    * [Feature/weather] UPDATE : weather 아이콘 구현
    
    * [feature/weather] UPDATE : weather 프론트엔드-백엔드 연동, 날씨 아이콘 에러
    
    * UPDATE : 날짜 데이터 바인딩 완료
    
    * UPDATE : 조건부 렌더링 사용하여 날씨 아이콘 구현 완료
    
    Co-authored-by: SangEunPark23 <101485039+SangEunPark23@users.noreply.github.com>
    Co-authored-by: ssook <60427387+ssook1222@users.noreply.github.com>
    Co-authored-by: ssook1222 <chlthr1222@naver.com>

--- tmeet-frontend/src/pages/meetingCheck/meetingChoice/MeetingChoice.jsx
+++ tmeet-frontend/src/pages/meetingCheck/meetingChoice/MeetingChoice.jsx
@@ -2,11 +2,32 @@ import NavBar from "../../components/navigationBar/NavBar";
 import React from "react";
 import KakaoShareButton from "../../components/KakaoShare/KakaoShare";
 import './Choice.css'
+import Box from "@mui/material/Box";
+import WeatherIcon from "../../components/weather/WeatherRe.tsx";
 
-let select = "모임 날짜 및 시간",
-    startdate1 = "YYYY-MM-DD AM/PM 00:00",
-    enddate1 = "YYYY-MM-DD AM/PM 00:00";
+let count = 0;
+var weatherList =
+    [
+        { date: '20220815', weather: '', rain: '강수없음', snow: '적설없음' },
+        { date: '20220816', weather: '', rain: '강수없음', snow: '적설없음' },
+        { date: '20220817', weather: '', rain: '강수없음', snow: '적설없음' },
+        { date: '20220818', weather: '구름많음', rain: '', snow: '' },
+        { date: '20220819', weather: '흐리고 비', rain: '', snow: '' },
+        {},
+        {},
+        {},
+        {},
+        {},
+        {}
+    ];
 
+for(let i = 0;i<weatherList.length ; i++){
+    if(weatherList[i].date == null){
+        count++;
+    }
+}
+
+let select = "모임 날짜 및 시간", startdate1 = weatherList[0].date, enddate1 = weatherList[count-2].date;
 
 function ChoiceApp() {
     return (
@@ -18,20 +39,44 @@ function ChoiceApp() {
             <div className="checkTitle">
                 모임 결과 확인
             </div>
-            <h2 style={{margin : "0", textAlign : "center"}}>최종으로 선택된 모임입니다.<br></br>변경하시려면 ㅇㅇ을 눌러주세요</h2>
+            <h2 style={{margin : "0", textAlign : "center"}}>최종으로 선택된 모임입니다.<br></br>변경하시려면 비동의 이모티콘을 입력해주세요</h2>
         </div>
-        <div>
-            <div className= "checkBoxStyle">
-                <h4 style={{margin : "auto"}}>{select}<br></br>{startdate1}<br></br>~{enddate1}</h4>
-            </div>
-            <div style={{margin : "1vh"}}></div>
-            <div className= "weatherBox">
+        {/*박스1*/}
+        <div className= "checkBoxMargin" style={{display : "flex", flexWrap : "wrap", justifyContent : "center"}}>
+            <div>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        backgroundColor : '#E3FDFD',
+                        color : '#46B3B9',
+                        width : '40vh',
+                        height : '15vh'
+                    }}>
+                    <h4 style={{margin : "auto"}}>{select}<br></br>{startdate1}<br></br> ~ {enddate1}</h4>
+                </Box>
+                <div style={{margin : "0.5vh"}}></div>
+                <Box className="weatherBox"
+                     sx={{
+                         display: 'flex',
+                         flexDirection: 'column',
+                         alignItems: 'center',
+                         backgroundColor : '#E3FDFD',
+                         width : '40vh',
+                         height : 'auto',
+                         overflow : "scroll"
+                     }}>
+                    <WeatherIcon></WeatherIcon>
+                </Box>
             </div>
         </div>
         <div style={{marginTop : "5vh", display : "block", textAlign : "center"}}>
             <div className= "kakaoShare">
                 결정된 모임을 카카오톡으로 공유하기
                 <KakaoShareButton></KakaoShareButton>
+            </div>
+            <div style={{backgroundColor : '#E3FDFD'}}>
             </div>
         </div>
         </body>
