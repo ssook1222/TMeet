0d4b7eb08f8640a8aa0cafea4534302ab7d4d5af
lucy1287 <80579765+lucy1287@users.noreply.github.com>
2022-08-29 14:35:06 +0900

    [Feature/time] UPDATE; 스케쥴러 간격 30분으로 조정 (#67)
    * UPDATE : timeresult 페이지 기능 구현
    
    * Merge branch 'feature/time' of C:\TMeet with conflicts.
    
    * UPDATE : time db 저장
    
    * UPDATE : timeresult 페이지 프론트, 백엔드 바인딩
    
    * [Feature/time] 각 사용자별 시간대 확인 기능 구현, UI 수정
    
    * [Feature/time] meeting 테이블 추가
    
    * [Feature/time] 버튼 css 수정
    
    * [Feature/time] 버튼 css 수정 .
    
    * [Feature/time] 버튼 css 수정 ..
    
    * [Feature/time] user, meeting 관계 설정
    
    * UPDATE : Time-User 관계 설정
    
    * [Feature/time] UPDATE: 스케쥴러 간격 30분으로 조정
    
    * [Feature/time] UPDATE: 스케쥴러 간격 30분으로 조정
    
    Co-authored-by: ssook1222 <chlthr1222@naver.com>
    Co-authored-by: ssook <60427387+ssook1222@users.noreply.github.com>

--- tmeet-frontend/src/pages/time/Time.jsx
+++ tmeet-frontend/src/pages/time/Time.jsx
@@ -11,7 +11,7 @@ function Time() {
       </div>
 
       <div className="timeTable">
-        <h3>가능한 시간대를 드래그하여 선택하세요.</h3>
+        <h3 id="h3">가능한 시간대를 드래그하여 선택하세요.</h3>
         <TimeTable></TimeTable>
       </div>
     </div>
